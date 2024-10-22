72ba8df2a8030f0a318c64080496fcbb448f756d
Raniwis <rosemary.9816@hotmail.com>
2024-10-22 18:10:32 -0300

    Update

--- step_07/tpot_clinical_pipeline.py
+++ step_07/tpot_clinical_pipeline.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import train_test_split
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
@@ -9,8 +9,8 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=42)
 
-# Average CV score on the training set was: 0.6962962962962963
-exported_pipeline = RandomForestClassifier(bootstrap=True, criterion="entropy", max_features=0.4, min_samples_leaf=13, min_samples_split=7, n_estimators=100)
+# Average CV score on the training set was: 0.7037037037037036
+exported_pipeline = GradientBoostingClassifier(learning_rate=0.1, max_depth=4, max_features=0.8, min_samples_leaf=11, min_samples_split=8, n_estimators=100, subsample=0.7000000000000001)
 # Fix random state in exported estimator
 if hasattr(exported_pipeline, 'random_state'):
     setattr(exported_pipeline, 'random_state', 42)
--- step_07/tpot_mutation_pipeline.py
+++ step_07/tpot_mutation_pipeline.py
@@ -1,7 +1,10 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+from tpot.builtins import ZeroCount
+from xgboost import XGBClassifier
+from tpot.export_utils import set_param_recursive
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -9,11 +12,13 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=42)
 
-# Average CV score on the training set was: 0.5460317460317461
-exported_pipeline = ExtraTreesClassifier(bootstrap=True, criterion="entropy", max_features=0.8, min_samples_leaf=19, min_samples_split=5, n_estimators=100)
-# Fix random state in exported estimator
-if hasattr(exported_pipeline, 'random_state'):
-    setattr(exported_pipeline, 'random_state', 42)
+# Average CV score on the training set was: 0.5682539682539682
+exported_pipeline = make_pipeline(
+    ZeroCount(),
+    XGBClassifier(learning_rate=0.1, max_depth=2, min_child_weight=13, n_estimators=100, n_jobs=1, subsample=0.6000000000000001, verbosity=0)
+)
+# Fix random state for all the steps in exported pipeline
+set_param_recursive(exported_pipeline.steps, 'random_state', 42)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
